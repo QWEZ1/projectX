4f102b87ce657b2d4fac3651f65110856221d425
QWEZ1 <126477631+QWEZ1@users.noreply.github.com>
2024-01-19 16:46:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const myButton = document.querySelector("#myButton");
+const nsmeInput = document.querySelector("#nameInput");
+const list = document.querySelector("#list");
+
+myButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("текст");
+})
